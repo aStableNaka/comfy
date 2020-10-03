9d9f5d53b03453aa805f6feb6a3b386e89722c14
Daniel <trandap88@gmail.com>
2020-10-03 15:58:02 -0400

    Automated Push

--- src/Auth.ts
+++ src/Auth.ts
@@ -2,7 +2,12 @@ import * as JWT from "jsonwebtoken";
 import * as HTTPS from "https";
 import { IncomingMessage } from "http";
 
-export async function Authenticate( token: string ): Promise<void>{
+
+/**
+ * 
+ * @param token API Token
+ */
+export async function Authenticate( token: string, callback:( res: IncomingMessage )=>{} ): Promise<void>{
 	const authOptions = {
 		hostname: "comfy.auth.na-ka.us",
 		port: 8080,
@@ -12,6 +17,7 @@ export async function Authenticate( token: string ): Promise<void>{
 
 	const res = await new Promise(( resolve: ( res:IncomingMessage )=>void, reject: ()=>void )=>{
 		HTTPS.request( authOptions, ( res: IncomingMessage )=>{
+			console.warn( res );
 			resolve( res );
 		});
 	});
